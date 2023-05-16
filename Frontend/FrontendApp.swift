ec2726770d1b52074e3f524a6bea17d94c65ffe3
ART7HUR <122246856+ART7HUR@users.noreply.github.com>
2023-05-17 01:55:15 +0900

    Initial Commit

---
+++ Frontend/FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendApp.swift
+//  Frontend
+//
+//  Created by 임승현 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
